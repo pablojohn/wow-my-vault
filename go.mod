3649684ba02d144671aab6aa5d127e7e3cd93539
pablojohn martinez <pablojohn.martinez@gmail.com>
2024-12-17 20:13:58 -0600

    wip: basic go app making API call

---
+++ go.mod
@@ -0,0 +1,3 @@
+module wowmyvault
+
+go 1.23.4
---
+++ wowmyvault.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+)
+
+func main() {
+	resp, err := http.Get("https://jsonplaceholder.typicode.com/posts")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	//We Read the response body on the line below.
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	//Convert the body to type string
+	sb := string(body)
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, sb, r.URL.Path)
+	})
+
+	http.ListenAndServe(":80", nil)
+}

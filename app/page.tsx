f9cd7bdb1dec47e8d3e9057cd32e728345bac50a
pablojohn martinez <pablojohn.martinez@gmail.com>
2024-12-19 19:05:44 -0600

    wip: working on updating the display

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Card } from './components/card';
 import Particles from "./components/particles";
 
 interface Dungeon {
@@ -20,6 +21,21 @@ export default async function Home() {
   if (weeklyDungeons.length >= 8)
     dungeonSlotsOpened = 3
 
+  const slots = [
+    {
+      mythic_level: 12,
+      reward_level: 623
+    },
+    {
+      mythic_level: 12,
+      reward_level: 623
+    },
+    {
+      mythic_level: 12,
+      reward_level: 623
+    }
+  ];
+
   return (
     <div className="flex flex-col items-center justify-center w-screen h-screen overflow-hidden bg-gradient-to-tl from-black via-zinc-600/20 to-black">
       <div className="hidden w-screen h-px animate-glow md:block animate-fade-left bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
@@ -28,9 +44,17 @@ export default async function Home() {
         quantity={100}
       />
       <h1 className="py-3.5 px-0.5 z-10 text-4xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text ">
-        {dungeonSlotsOpened}
+        <div className="grid w-full grid-cols-1 gap-8 mx-auto mt-32 sm:mt-0 sm:grid-cols-3 lg:gap-16">
+          {slots.map((slot, index) => (
+            <Card>
+              <div className="flex flex-col items-center justify-center w-full h-full p-4 space-y-4">
+                <span className="text-2xl text-zinc-500">{slot.mythic_level}</span>
+                <span className="text-2xl text-zinc-500">{slot.reward_level}</span>
+              </div>
+            </Card>
+          ))}
+        </div>
       </h1>
-
       <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
       <div className="my-16 text-center animate-fade-in">
         <h2 className="text-sm text-zinc-500 ">
